f1ef0151c5e19a02e20294df8f32896411a1e540
Krilon <mgustafsson@gmail.com>
2016-06-25 01:29:19 +0200

    DB
    Put in code for DB access.

---
+++ app/src/main/java/com/example/mcmac/kompistippning/AbstractBettingDB.java
@@ -0,0 +1,150 @@
+package com.example.mcmac.kompistippning;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.sql.SQLException;
+
+/**
+ * Created by Mattias on 2016-01-06.
+ */
+public abstract class AbstractBettingDB {
+
+
+    //Competitionstable
+    public static final String DB_TABLE_COMPETITIONS ="competitions";
+    public static final String KEY_EVENT_NAME = "event_name";
+
+    //Gamestable
+    public static final String DB_TABLE_GAMES = "games";
+    public static final String KEY_DATE = "date";
+    public static final String KEY_TEAM_A ="team_a";
+    public static final String KEY_TEAM_B ="team_b";
+    public static final String KEY_GAME_ID ="game_id";
+    public static final String KEY_BET_AMOUNT= "bet_amount";
+    public static final String KEY_GAME_TYPE = "game_type";
+
+    //Participantstable
+    public static final String DB_TABLE_PARTICIPANTS = "participants";
+    public static final String KEY_PERSON = "person";
+
+    //Betstable
+    public static final String DB_TABLE_BETS = "bets";
+    public static final String KEY_BET="bet";
+/*
+    //Kommuntabellen
+    public static final String DB_TABLE_MUNICIP ="municip";
+    public static final String KEY_MUNICIP_ID ="municip_id";
+    public static final String KEY_MUNICIP_NAME="municip_name";
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_PHONE = "phone";
+
+    //Händelsetabellen
+    public static final String DB_TABLE_EVENT ="event";
+    public static final String KEY_EVENT_ID = "event_id";
+    //public static final String KEY_EVENT_NAME = "event_name";
+    public static final String KEY_EVENT_GRADE = "event_grade";
+    public static final String KEY_EVENT_PLACE = "event_place";
+    public static final String KEY_EVENT_DESCRIPTION ="event_description";
+    public static final String KEY_EVENT_STATUS ="event_status";
+    public static final String KEY_EVENT_DATE = "event_date";
+    public static final String DB_NAME = "municipalities";
+*/
+    public static final String DB_NAME = "competitionbets";
+    protected SQLiteDatabase database;
+
+    private static final String [] competitions_columns = {KEY_EVENT_NAME};
+    private static final String [] games_columns = {KEY_EVENT_NAME,KEY_DATE, KEY_TEAM_A, KEY_TEAM_B, KEY_BET_AMOUNT, KEY_GAME_TYPE};
+    private static final String [] participants_columns = {KEY_EVENT_NAME, KEY_PERSON};
+    private static final String [] bets_columns= {KEY_EVENT_NAME, KEY_PERSON, KEY_GAME_ID, KEY_BET};
+
+    //private static final String [] municip_columns = {KEY_MUNICIP_ID,KEY_MUNICIP_NAME, KEY_EMAIL, KEY_PHONE};
+    //private static final String [] event_columns = {KEY_EVENT_ID, KEY_EVENT_NAME, KEY_MUNICIP_NAME, KEY_EVENT_GRADE, KEY_EVENT_PLACE, KEY_EVENT_DESCRIPTION, KEY_EVENT_STATUS, KEY_EVENT_DATE};
+
+    private static final int DB_VERSION = 1;
+    private MyDbHelper myDbHelper;
+    private Context context;
+
+    //Strängar för skapande av de fyra tabellerna
+    private static final String DB_CREATE_COMPETITIONS = "create table " + DB_TABLE_COMPETITIONS + " (" +KEY_EVENT_NAME +" text not null );";
+    private static final String DB_CREATE_GAMES = "create table " + DB_TABLE_GAMES + " (" + KEY_EVENT_NAME +" text not null, " +KEY_DATE +" text not null, " +KEY_TEAM_A +" text not null, " + KEY_TEAM_B + " text not null, " + KEY_GAME_ID + " integer primary key autoincrement, " +KEY_BET_AMOUNT +" text not null, " +KEY_GAME_TYPE +" text not null );";
+    private static final String DB_CREATE_PARTICIPANTS = "create table " + DB_TABLE_PARTICIPANTS + " (" +KEY_EVENT_NAME + " text not null, " + KEY_PERSON +" text not null );";
+    private static final String DB_CREATE_BETS = "create table " + DB_TABLE_BETS + " (" +KEY_EVENT_NAME + " text not null," +KEY_PERSON +" text not null, "+ KEY_GAME_ID + " text not null, " + KEY_BET + " text not null );";
+    //private static final String DB_CREATE_MUNICIP = "create table " + DB_TABLE_MUNICIP + " (" +KEY_MUNICIP_ID + " integer primary key autoincrement, " + KEY_MUNICIP_NAME + " text not null, " + KEY_EMAIL + " text not null, " + KEY_PHONE + " text not null );";
+    //private static final String DB_CREATE_EVENT = "create table " + DB_TABLE_EVENT + " (" +KEY_EVENT_ID + " integer primary key autoincrement, "+ KEY_EVENT_NAME + " text not null, " + KEY_MUNICIP_NAME + " text not null, " + KEY_EVENT_GRADE + " text not null, "+ KEY_EVENT_PLACE + " text not null, " + KEY_EVENT_DESCRIPTION + " text not null, "+ KEY_EVENT_STATUS + " text not null, " + KEY_EVENT_DATE + " text not null );";
+
+
+    protected abstract void createTestData(); //Must be implemented
+
+    public void open(boolean writable, Context ctx) throws SQLException {
+        this.context = ctx;
+        myDbHelper= new MyDbHelper(context);
+        if (writable){
+            database =myDbHelper.getWritableDatabase();
+            if (myDbHelper.isFirstTime())
+                createTestData();
+        }
+        else
+            database =myDbHelper.getReadableDatabase();
+    }
+
+    public void close(){
+        myDbHelper.close();
+    }
+/*
+    protected Cursor getAllMunicipsCursor(){ //select * from Municip
+        return database.query(DB_TABLE_MUNICIP, municip_columns, null, null, null, null, null);
+    }
+
+    protected Cursor getAllEventPointsCursor(){ //select * from EventPoint
+        return database.query(DB_TABLE_EVENT, event_columns, null, null, null, null, null);
+    }
+    */
+
+    protected Cursor getAllCompetitionsCursor(){ //select * from Competitions
+        return database.query(DB_TABLE_COMPETITIONS,competitions_columns, null, null, null, null, null);
+    }
+
+    protected Cursor getAllGamesCursor(){ //select * from Games
+        return database.query(DB_TABLE_GAMES,games_columns, null, null, null, null, null);
+    }
+
+    protected Cursor getAllParticipantsCursor(){ //select * from Participants
+        return database.query(DB_TABLE_PARTICIPANTS,participants_columns, null, null, null, null, null);
+    }
+
+    protected Cursor getAllBetsCursor(){ //select * from Bets
+        return database.query(DB_TABLE_BETS,bets_columns, null, null, null, null, null);
+    }
+
+    protected static class MyDbHelper extends SQLiteOpenHelper { //Local help class
+        private boolean firstTime = false;
+        MyDbHelper(Context context){
+            super(context, DB_NAME, null, DB_VERSION);
+        }
+        public void onCreate(SQLiteDatabase sqldb){
+//            sqldb.execSQL(DB_CREATE_MUNICIP);
+//            sqldb.execSQL(DB_CREATE_EVENT);
+            sqldb.execSQL(DB_CREATE_COMPETITIONS);
+            sqldb.execSQL(DB_CREATE_GAMES);
+            sqldb.execSQL(DB_CREATE_PARTICIPANTS);
+            sqldb.execSQL(DB_CREATE_BETS);
+            firstTime =true;
+        }
+
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+        //    db.execSQL("DROP TABLE IF EXISTS " + DB_TABLE_MUNICIP);
+        //    db.execSQL("DROP TABLE IF EXISTS " + DB_TABLE_EVENT);
+            db.execSQL("DROP TABLE IF EXISTS " + DB_TABLE_COMPETITIONS);
+            db.execSQL("DROP TABLE IF EXISTS " + DB_TABLE_GAMES);
+            db.execSQL("DROP TABLE IF EXISTS " + DB_TABLE_PARTICIPANTS);
+            db.execSQL("DROP TABLE IF EXISTS " + DB_TABLE_BETS);
+            onCreate(db);
+        }
+        public boolean isFirstTime(){
+            return firstTime;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mcmac/kompistippning/BettingDB.java
@@ -0,0 +1,222 @@
+package com.example.mcmac.kompistippning;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Mattias on 2016-06-24.
+ */
+
+public class BettingDB extends AbstractBettingDB{
+
+    private static final BettingDB theOnlyDB = new BettingDB(); //Singleton
+
+    private BettingDB() {  //Private due to Singleton pattern
+
+    }
+
+    public static BettingDB getInstance(){ //Call this to get the DB object where ever in the app
+        return theOnlyDB;
+    }
+
+    public Competition insertCompetition (String event_name){
+        ContentValues values = new ContentValues();
+        values.put(KEY_EVENT_NAME, event_name);
+        long rowId = database.insert(DB_TABLE_COMPETITIONS, null, values);
+        return new Competition(rowId, event_name);
+    }
+
+    public Game insertGame(String event_name, String date, String team_A, String team_B, String bet_amounts, String game_type ){
+        ContentValues values = new ContentValues();
+        values.put(KEY_EVENT_NAME, event_name);
+        values.put(KEY_DATE, date);
+        values.put(KEY_TEAM_A, team_A);
+        values.put(KEY_TEAM_B, team_B);
+        values.put(KEY_BET_AMOUNT, bet_amounts);
+        values.put(KEY_GAME_TYPE, game_type);
+        long rowId = database.insert(DB_TABLE_GAMES, null, values);
+        return new Game(rowId, event_name, date, team_A, team_B, bet_amounts, game_type );
+    }
+
+    public Participant insertParticipant(String event_name, String person){
+        ContentValues values = new ContentValues();
+        values.put(KEY_EVENT_NAME, event_name);
+        values.put(KEY_PERSON, person);
+        long rowId = database.insert(DB_TABLE_PARTICIPANTS, null, values);
+        return new Participant(rowId, event_name, person);
+    }
+    public Bet insertBet(String event_name, String person, String game_id, String bet){
+        ContentValues values = new ContentValues();
+        values.put(KEY_EVENT_NAME, event_name);
+        values.put(KEY_PERSON, person);
+        values.put(KEY_GAME_ID, game_id);
+        values.put(KEY_BET, bet);
+        long rowID = database.insert(DB_TABLE_BETS, null, values);
+        return new Bet(rowId, event_name, person);
+    }
+
+    /*
+    public Municip insertMunicip(String municip, String email, String phone){
+        ContentValues values = new ContentValues();
+        values.put(KEY_MUNICIP_NAME, municip);
+        values.put(KEY_EMAIL, email);
+        values.put(KEY_PHONE, phone);
+        long rowId = database.insert(DB_TABLE_MUNICIP, null, values);
+        Log.v("MyTag", "inserts");
+        return new Municip(rowId, municip, email, phone);
+    }
+
+    public EventPoint insertEventPoint(String event, String municip, String grade, String place, String description, String status, String date){
+        ContentValues values = new ContentValues();
+        values.put(KEY_EVENT_NAME, event);
+        values.put(KEY_MUNICIP_NAME, municip);
+        values.put(KEY_EVENT_GRADE, grade);
+        values.put(KEY_EVENT_PLACE, place);
+        values.put(KEY_EVENT_DESCRIPTION, description);
+        values.put(KEY_EVENT_STATUS, status);
+        values.put(KEY_EVENT_DATE, date);
+        long rowId = database.insert(DB_TABLE_EVENT, null, values);
+        Log.v("MyTag",String.valueOf(rowId));
+        return new EventPoint(rowId, event, municip, grade, place, description, status, date);
+    }*/
+
+    /* Detta ska implementeras senare för KompisBetting
+    public boolean updateMunicip(Municip municip){
+        ContentValues values = new ContentValues();
+        values.put(KEY_MUNICIP_NAME, municip.getMunicip());
+        values.put(KEY_EMAIL, municip.getEmail());
+        values.put(KEY_PHONE, municip.getPhone());
+        Log.v("MyTag", "updates");
+        return database.update(DB_TABLE_MUNICIP, values, KEY_MUNICIP_ID + "=" + municip.getId(), null) > 0;
+    }
+
+    public boolean updateEventPoint(EventPoint event){
+        ContentValues values = new ContentValues();
+        values.put(KEY_EVENT_NAME, event.getEvent());
+        values.put(KEY_MUNICIP_NAME, event.getMunicip());
+        values.put(KEY_EVENT_GRADE, event.getGrade());
+        values.put(KEY_EVENT_PLACE, event.getPlace());
+        values.put(KEY_EVENT_DESCRIPTION, event.getDescription());
+        values.put(KEY_EVENT_STATUS, event.getStatus());
+        values.put(KEY_EVENT_DATE, event.getDate());
+        return database.update(DB_TABLE_EVENT, values, KEY_EVENT_ID + "=" + event.getId(), null) > 0;
+    }*/
+
+    /*Detta ska implementeras senare för KompisBetting
+    public boolean deleteMunicip(Municip municip){
+        return database.delete(DB_TABLE_MUNICIP, KEY_MUNICIP_ID + "=" + municip.getId(), null) > 0;
+    }
+    public boolean deleteEventPoint(EventPoint event){
+        return database.delete(DB_TABLE_EVENT, KEY_EVENT_ID + "=" + event.getId(), null) > 0;
+    }
+*/
+
+    public ArrayList<Competition> getAllCompetitions(){
+        Cursor cr = getAllCompetitionsCursor();
+        return makeCompetitionListFromCursor(cr);
+    }
+    public ArrayList<Game> getAllGames(){
+        Cursor cr = getAllGamesCursor();
+        return makeGameListFromCursor(cr);
+    }
+    public ArrayList<Participant> getAllParticipants(){
+        Cursor cr = getAllParticipantsCursor();
+        return makeParticipantListFromCursor(cr);
+    }
+    public ArrayList<Bet> getAllBets(){
+        Cursor cr = getAllBetsCursor();
+        return makeBetListFromCursor(cr);
+    }
+
+    private ArrayList<Competition> makeCompetitionListFromCursor(Cursor cr){
+        ArrayList<Competition> competitions = new ArrayList<Competition>();
+        if (cr != null && cr.moveToFirst())
+            do {
+                competitions.add(new Competition(cr.getString(0)));
+            } while (cr.moveToNext());
+        return competitions;
+    }
+
+    private ArrayList<Game> makeGameListFromCursor(Cursor cr){
+        ArrayList<Game> games = new ArrayList<Game>();
+        if (cr != null && cr.moveToFirst())
+            do {
+                games.add(new Game(cr.getString(0), cr.getString(1), cr.getString(2), cr.getString(3), cr.getString(4), cr.getString(5), cr.getString(6)));
+            } while (cr.moveToNext());
+        return games;
+    }
+
+    private ArrayList<Participant> makeParticipantListFromCursor(Cursor cr){
+        ArrayList<Participant> participants = new ArrayList<Participant>();
+        if (cr != null && cr.moveToFirst())
+            do {
+                participants.add(new Participant(cr.getString(0), cr.getString(1)));
+            } while (cr.moveToNext());
+        return participants;
+    }
+
+    private ArrayList<Bet> makeBetListFromCursor(Cursor cr){
+        ArrayList<Bet> bets = new ArrayList<Bet>();
+        if (cr != null && cr.moveToFirst())
+            do {
+                bets.add(new Bet(cr.getString(0), cr.getString(1), cr.getString(2), cr.getString(3)));
+            } while (cr.moveToNext());
+        return bets;
+    }
+
+/*
+    public ArrayList<Municip> getAllMunicips() {
+        Cursor cr = getAllMunicipsCursor();
+        return makeMunicipListFromCursor(cr);
+    }
+    public ArrayList<EventPoint> getAllEventPoints() {
+        Cursor cr = getAllEventPointsCursor();
+        return makeEventPointListFromCursor(cr);
+    }
+
+    private ArrayList<Municip> makeMunicipListFromCursor(Cursor cr){
+        ArrayList<Municip> municips = new ArrayList<Municip>();
+        if (cr != null && cr.moveToFirst())
+            do {
+                municips.add(new Municip(cr.getInt(0), cr.getString(1), cr.getString(2), cr.getString(3)));
+            } while (cr.moveToNext());
+        return municips;
+    }
+
+    private ArrayList<EventPoint> makeEventPointListFromCursor(Cursor cr){
+        ArrayList<EventPoint> events = new ArrayList<EventPoint>();
+        if (cr != null && cr.moveToFirst())
+            do {
+                events.add(new EventPoint(cr.getInt(0), cr.getString(1), cr.getString(2), cr.getString(3), cr.getString(4), cr.getString(5),cr.getString(6), cr.getString(7)));
+            } while (cr.moveToNext());
+        return events;
+    }*/
+
+    protected void createTestData() {
+        //Competition-table
+        BettingDB.getInstance().insertCompetition("EM 2016");
+
+        //Games-table
+        BettingDB.getInstance().insertGame("EM 2016","2016-06-25","Kroatien", "Portugal","5","Åttondel");
+
+        //Participant-table
+        BettingDB.getInstance().insertParticipant("EM 2016", "David");
+
+        //Bet-table
+        BettingDB.getInstance().insertBet("EM 2016", "David", "0", "1-2");
+
+        /*
+        //Municip-table
+        MunicipDB.getInstance().insertMunicip("Goteborgs kommun", "goteborg@testdata.se", "031-XXXXX");
+        MunicipDB.getInstance().insertMunicip("Vaggeryds kommun", "vaggeryd@testdata.se", "0393-XXXXX");
+        MunicipDB.getInstance().insertMunicip("Molndals kommun", "molndal@testdata.se", "031-2XXXXX");
+
+        //EventPoint-table
+        MunicipDB.getInstance().insertEventPoint("Trasig lampa", "Vaggeryds kommun", "Allvarlig", "Koord", "Kvall i december", "false", "2015-12-10");
+        MunicipDB.getInstance().insertEventPoint("Trasig jarnvagsbom", "Göteborgs kommun", "Mycket allvarlig", "Koord", "Kvall i december", "true", "2015-12-24");
+    */
+    }
+
+}
