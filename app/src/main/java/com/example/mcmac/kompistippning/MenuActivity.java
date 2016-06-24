69efaea47c7eb5794210543b1dd2bab14aa5efd0
Sandra Derbring <sandra.derbring@gmail.com>
2016-06-25 01:10:19 +0200

    "Layout draft"

--- app/src/main/java/com/example/mcmac/kompistippning/MenuActivity.java
+++ app/src/main/java/com/example/mcmac/kompistippning/MenuActivity.java
@@ -1,7 +1,9 @@
 package com.example.mcmac.kompistippning;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -10,4 +12,30 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
     }
+
+    public void onClickBet(View button) {
+
+        Intent intent = new Intent(this, BetActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickAdd(View button) {
+
+        Intent intent = new Intent(this, AddActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickCompetitions(View button) {
+
+        Intent intent = new Intent(this, CompetitionActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickStatistics(View button) {
+
+        Intent intent = new Intent(this, StatisticsActivity.class);
+        startActivity(intent);
+    }
+
+
 }
