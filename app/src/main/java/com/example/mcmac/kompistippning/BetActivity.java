01610531a024b7c9656c8b6a75e111f90bc10f68
Krilon <mgustafsson@gmail.com>
2016-06-25 23:48:29 +0200

    "Added MyItemListener"

--- app/src/main/java/com/example/mcmac/kompistippning/BetActivity.java
+++ app/src/main/java/com/example/mcmac/kompistippning/BetActivity.java
@@ -3,9 +3,12 @@ package com.example.mcmac.kompistippning;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.sql.SQLException;
 
@@ -32,17 +35,48 @@ public class BetActivity extends AppCompatActivity {
             Log.e("MyTag", e.getMessage(), e);
         }
 
+        MyItemSelectedListener myItemSelectedListener= new MyItemSelectedListener();
+
         competitionSpinner = (Spinner) findViewById(R.id.competitionSpinner);
         competitionAdapter = new ArrayAdapter<Competition>(this, android.R.layout.simple_spinner_item, android.R.id.text1, BettingDB.getInstance().getAllCompetitions());
         competitionSpinner.setAdapter(competitionAdapter);
+        competitionSpinner.setOnItemSelectedListener(myItemSelectedListener);
 
         gameSpinner = (Spinner) findViewById(R.id.gameSpinner);
         gameAdapter = new ArrayAdapter<Game>(this, android.R.layout.simple_spinner_item, android.R.id.text1, BettingDB.getInstance().getAllGames());
         gameSpinner.setAdapter(gameAdapter);
+        gameSpinner.setOnItemSelectedListener(myItemSelectedListener);
 
         participantSpinner = (Spinner) findViewById(R.id.participantSpinner);
         participantAdapter = new ArrayAdapter<Participant>(this, android.R.layout.simple_spinner_item, android.R.id.text1, BettingDB.getInstance().getAllParticipants());
         participantSpinner.setAdapter(participantAdapter);
+        participantSpinner.setOnItemSelectedListener(myItemSelectedListener);
+
+
 
     }
+
+    private class MyItemSelectedListener implements AdapterView.OnItemSelectedListener {
+        public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+            /*
+            Municip municip = (Municip)municipSpinner.getSelectedItem();
+            emailShowText.setText(municip.getEmail());
+            System.out.println(municip);
+            event_choice = (String)eventSpinner.getSelectedItem();
+            System.out.println(event_choice);
+            grade_choice = (String)gradeSpinner.getSelectedItem();
+            System.out.println(grade_choice);
+            */
+            TextView teamATextView =(TextView)findViewById(R.id.teamATextView);
+            Game gameA = (Game)gameSpinner.getSelectedItem();
+            teamATextView.setText(gameA.getTeamA());
+
+            TextView teamBTextView =(TextView)findViewById(R.id.teamBTextView);
+            Game gameB = (Game)gameSpinner.getSelectedItem();
+            teamBTextView.setText(gameB.getTeamB());
+
+
+        }
+        public void onNothingSelected(AdapterView<?> parent) {
+        }};
 }
